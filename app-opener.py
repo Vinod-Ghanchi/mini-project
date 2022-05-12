8ee40f74f79a04610d04076e476e599a42782d6f
Vinod-Ghanchi <80514865+Vinod-Ghanchi@users.noreply.github.com>
2022-05-12 21:34:20 +0530

    Update app-opener.py

--- app-opener.py
+++ app-opener.py
@@ -88,5 +88,16 @@ while True:
         #
         #     xp, yp = x1, y1
 
-    
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)
+    img = cv2.bitwise_or(img, imgCanvas)
+
+    cv2.imshow("Image", img)
+    if (cv2.waitKey(30) == ord('q')):
+        file_name_path = 'C:/Users/kushl/PycharmProjects/MIniProject/' +  '.jpg'
+        cv2.imwrite(file_name_path, imgInv)
+        break
+cap.release()
 
